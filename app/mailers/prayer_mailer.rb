d926ce249e047256464706f3ffaee2aa6a8df1e7
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-06-22 11:32:36 -0500

    footer

--- app/mailers/prayer_mailer.rb
+++ app/mailers/prayer_mailer.rb
@@ -3,4 +3,9 @@ class PrayerMailer < ActionMailer::Base
   	@prayer = prayer
   	mail(from: @prayer.email, to: 'aaronjohnsonis@gmail.com', subject: 'Prayer Request')
   end
+
+  def prayer_request_received(prayer)
+  	@prayer = prayer
+  	mail(from: 'aaronjohnsonis@gmail.com', to: @prayer.email, subject: 'Prayer Request Received')
+  end
 end
