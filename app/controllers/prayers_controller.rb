d6c572494a9cbb6e45caf28d5f40b3eea30552d8
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-06-22 19:44:08 -0500

    email

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -4,7 +4,10 @@ class PrayersController < ApplicationController
     @prayer = Prayer.create(prayer_params)
     if @prayer.save
       PrayerMailer.prayer_request(@prayer).deliver
+      PrayerMailer.prayer_request_received(@prayer).deliver
       redirect_to root_path
+    else
+      refirect_to root_path
     end
   end
 
